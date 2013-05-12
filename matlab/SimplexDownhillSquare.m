26fc526ae38ded87cf465e4e8a0018a03912f13b
Selina Malacarne <selina@swissonline.ch>
2013-05-12 22:02:55 +0200

    Beispiele noch reingetan selina

---
+++ matlab/SimplexDownhillSquare.m
@@ -0,0 +1,53 @@
+%Anwendung:     Simplex-Downhill Algorithmus
+%Datum:         03.05.2013
+%Author:        Selina Malacarne, Raphael Nestler
+close all; 
+clear all; 
+
+xBorder = 20; 
+iteration = 20; 
+
+x = (-xBorder:1:xBorder);
+y = (-xBorder:1:xBorder); 
+
+funSquare = square(x); 
+
+x_newSquare1 = 18; %Startwert
+y_newSquare1 = 0;
+x_valuesSquare1 = zeros(2*iteration+1); 
+y_valuesSquare1 = zeros(2*iteration+1); 
+
+x_newSquare2 = -18; %Startwert
+y_newSquare2 = 0;
+x_valuesSquare2 = zeros(2*iteration+1); 
+y_valuesSquare2 = zeros(2*iteration+1); 
+
+options = struct('MaxIter',2);
+
+for i=1:(2*iteration+1)
+    [x_newSquare1 y_newSquare1] = fminsearch(@square,x_newSquare1,options);
+    x_valuesSquare1(i) = x_newSquare1; 
+    y_valuesSquare1(i) = y_newSquare1;
+    
+    [x_newSquare2 y_newSquare2] = fminsearch(@square,x_newSquare2,options);
+    x_valuesSquare2(i) = x_newSquare2; 
+    y_valuesSquare2(i) = y_newSquare2;
+end
+ 
+figure; 
+plot(x,funSquare,'r','linewidth',2);
+xlabel('x')
+ylabel('y')
+hold on; 
+for i=1:length(x_valuesSquare1)
+ plot(x_valuesSquare1(i), y_valuesSquare1(i), '*b')
+  plot(x_valuesSquare2(i), y_valuesSquare2(i), '*k')
+ %pause(0.5)
+end
+hold off; 
+grid on; 
+set(gca, 'GridLineStyle', '-');
+grid(gca,'minor');
+
+
+ 
--- matlab/Simplex_Downhill.m
+++ matlab/Simplex_Downhill.m
@@ -5,80 +5,61 @@ close all;
 clear all; 
 
 xBorder = 10; 
+teration = 20; 
 
-x = (-xBorder:1:xBorder);
-yA = (-xBorder:1:xBorder); 
 
-xH = (0:0.1:3); 
-yH = (0:0.1:3); 
+x = (-xBorder:0.1:xBorder); 
+y = (-xBorder:0.1:xBorder); 
 
-funSquare = square(x); 
-[xx, yy] = meshgrid(xH,yH);
-funHimmelblau = rastrigin(xx,yy); 
+[xx, yy] = meshgrid(x,y);
+funHB = himmelblau(xx,yy); 
 
-xSquare = square(x);
-ySquare = square(yA);
-% zSquare = square(z); 
+x_HB1 = [9,9]; %Startwert
+z_HB1 = 0; 
+x_HB2 = [-9,-9]; %Startwert
+z_HB2 = 0; 
 
-funSphere = sphere(1,2,3); 
-%plot(x,fun);
-x_newSquare = 20; %Startwert
-y_newSquare = 0;
-x_valuesSquare = zeros(2*xBorder+1); 
-y_valuesSquare = zeros(2*xBorder+1); 
 
-x_newHimmelblau = [0.4,1]; %Startwert
+x_valHB1 = zeros(2,2*teration+1); 
+z_valHB1 = zeros(1,2*teration+1); 
+x_valHB2 = zeros(2,2*teration+1); 
+z_valHB2 = zeros(1,2*teration+1); 
 
-x_temp = [0 0];
-z_temp = 0; 
-x_valuesHimmelblau = zeros(2,2*xBorder+1); 
-z_valuesHimmelblau = zeros(1,2*xBorder+1); 
+options1 = struct('MaxIter',3);
+options2 = struct('MaxIter',6);
 
-options1 = struct('MaxIter',10,'PlotFcns',@optimplotfval);
-options2 = struct('MaxIter',3);
-%w = waitforbuttonpress; 
-
-for i=1:(2*xBorder+1)
-    [x_newSquare y_newSquare] = fminsearch(@square,x_newSquare,options2);
-    x_valuesSquare(i) = x_newSquare; 
-    y_valuesSquare(i) = y_newSquare;
-    
-     [x_newHimmelblau z_temp] = fminsearch(@rastrigin,x_newHimmelblau,options2);
-     x_valuesHimmelblau(1,i) = x_newHimmelblau(1,1);
-     x_valuesHimmelblau(2,i) = x_newHimmelblau(1,2); 
-     z_valuesHimmelblau(i) =  z_temp; 
+for i=1:(2*teration+1)
+     [x_HB1 z_HB1] = fminsearch(@himmelblau,x_HB1,options1);
+     x_valHB1(1,i) = x_HB1(1,1);
+     x_valHB1(2,i) = x_HB1(1,2); 
+     z_valHB1(i) =  z_HB1; 
+     
+     [x_HB2 z_HB2] = fminsearch(@himmelblau,x_HB2,options2);
+     x_valHB2(1,i) = x_HB2(1,1);
+     x_valHB2(2,i) = x_HB2(1,2); 
+     z_valHB2(i) =  z_HB2; 
 end
- 
-figure; 
-plot(x,funSquare,'r','linewidth',2);
-xlabel('y')
-ylabel('y')
-%title('Angular velocity with r = 1m')
-hold on; 
-for i=1:length(x_valuesSquare)
- plot(x_valuesSquare(i), y_valuesSquare(i), '*')
- pause(0.5)
-end
-hold off; 
-grid on; 
-% set(gca, 'GridLineStyle', '-');
-% grid(gca,'minor');
 
 figure; 
-grid on; 
-contour(xx,yy,funHimmelblau,50);
+contour(xx,yy,funHB,70);
 hold on; 
-for i=1:length(x_valuesHimmelblau)
-    plot(x_valuesHimmelblau(1,i),x_valuesHimmelblau(2,i),'-*r'); 
+for i=1:length(x_valHB1)
+    plot(x_valHB1(1,i),x_valHB1(2,i),'-*r'); 
+    plot(x_valHB2(1,i),x_valHB2(2,i),'-*k'); 
     pause(0.5)
 end
-
+xlabel('x1');
+ylabel('x2');
 hold off; 
 
 figure; 
-mesh(xx,yy,funHimmelblau);
+mesh(xx,yy,funHB);
 hold on; 
-plot3(x_valuesHimmelblau(1,:),x_valuesHimmelblau(2,:),z_valuesHimmelblau(:),'r','linewidth',4); 
+plot3(x_valHB1(1,:),x_valHB1(2,:),z_valHB1(:),'r','linewidth',4); 
+plot3(x_valHB2(1,:),x_valHB2(2,:),z_valHB2(:),'k','linewidth',4); 
 hold off; 
+xlabel('x1');
+ylabel('x2');
+zlabel('z');
 grid on; 
  
