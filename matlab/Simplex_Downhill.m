4532376aae290c3354b2de5b8a45226e16c2e075
Selina Malacarne <selina@swissonline.ch>
2013-05-13 16:00:05 +0200

    Praesenation Bilder reingetan selina

--- matlab/Simplex_Downhill.m
+++ matlab/Simplex_Downhill.m
@@ -5,7 +5,7 @@ close all;
 clear all; 
 
 xBorder = 10; 
-teration = 20; 
+iteration = 20; 
 
 
 x = (-xBorder:0.1:xBorder); 
@@ -20,21 +20,20 @@ x_HB2 = [-9,-9]; %Startwert
 z_HB2 = 0; 
 
 
-x_valHB1 = zeros(2,2*teration+1); 
-z_valHB1 = zeros(1,2*teration+1); 
-x_valHB2 = zeros(2,2*teration+1); 
-z_valHB2 = zeros(1,2*teration+1); 
+x_valHB1 = zeros(2,2*iteration+1); 
+z_valHB1 = zeros(1,2*iteration+1); 
+x_valHB2 = zeros(2,2*iteration+1); 
+z_valHB2 = zeros(1,2*iteration+1); 
 
-options1 = struct('MaxIter',3);
-options2 = struct('MaxIter',6);
+options = struct('MaxIter',3);
 
-for i=1:(2*teration+1)
-     [x_HB1 z_HB1] = fminsearch(@himmelblau,x_HB1,options1);
+for i=1:(2*iteration+1)
+     [x_HB1 z_HB1] = fminsearch(@himmelblau,x_HB1,options);
      x_valHB1(1,i) = x_HB1(1,1);
      x_valHB1(2,i) = x_HB1(1,2); 
      z_valHB1(i) =  z_HB1; 
-     
-     [x_HB2 z_HB2] = fminsearch(@himmelblau,x_HB2,options2);
+   
+     [x_HB2 z_HB2] = fminsearch(@himmelblau,x_HB2,options);
      x_valHB2(1,i) = x_HB2(1,1);
      x_valHB2(2,i) = x_HB2(1,2); 
      z_valHB2(i) =  z_HB2; 
@@ -46,8 +45,9 @@ hold on;
 for i=1:length(x_valHB1)
     plot(x_valHB1(1,i),x_valHB1(2,i),'-*r'); 
     plot(x_valHB2(1,i),x_valHB2(2,i),'-*k'); 
-    pause(0.5)
+    pause(0.1)
 end
+
 xlabel('x1');
 ylabel('x2');
 hold off; 
